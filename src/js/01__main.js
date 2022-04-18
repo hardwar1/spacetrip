297553d321815c79d34f953421e2170957c3855c
Serg k <xapgbap1@ya.ru>
2022-04-18 13:11:36 +0300

    first commit

---
+++ src/js/01__main.js
@@ -0,0 +1,6 @@
+$(function () {
+  $(".quote__slider").slick({
+    nextArrow: '<button type="button" class="quote__btn quote__btn--next"></button>',
+    prevArrow: '<button type="button" class="quote__btn quote__btn--prev"></button>'
+  });
+});
