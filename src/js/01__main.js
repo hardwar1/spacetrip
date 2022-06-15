151670d4cef56836569a2ae9b5ebd3c7f8447478
Serg k <xapgbap1@ya.ru>
2022-06-15 12:52:14 +0300

    адаптив, плавное  фиксированное меню, плавные скроллы к якорям

--- src/js/01__main.js
+++ src/js/01__main.js
@@ -30,6 +30,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   }
   // /мой мини jquery
 
+  // мобильное меню и бургер
   const
     burger = qOne('.burger'),
     menu = qOne('.menu');
@@ -42,5 +43,69 @@ document.addEventListener("DOMContentLoaded", function (event) {
   menu.addEventListener('click', function () {
     this.classList.remove('menu--close');
   });
+
+  // свайпер слайдер 
+
+  const swiper = new Swiper('.swiper', {
+    speed: 400,
+    spaceBetween: 100,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  // скроллы хеадера
+  const header = qOne('.header');
+  let scrollTop,
+    top = 0;
+
+  window.addEventListener('scroll', function () {
+    scrollTop = window.scrollY;
+
+    if (scrollTop > 100) {
+      header.classList.add('header--scroll');
+    } else {
+      header.classList.remove('header--scroll');
+    }
+
+    if (top < scrollTop && top > 500) {
+      header.classList.add('header--hide');
+      top = scrollTop;
+    } else {
+      header.classList.remove('header--hide');
+      top = scrollTop;
+    }
+  });
+
+  // скроллы якорных ссылок
+  const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+    animationTime = 1000,
+    framesCount = 200;
+
+  anchors.forEach(function (item) {
+    item.addEventListener('click', function (e) {
+      e.preventDefault();
+      let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+
+      let scroller = setInterval(function () {
+        // скорость прокрутки
+        let scrollBy = 17;
+        if (Math.abs(window.pageYOffset - coordY) < scrollBy) {
+          window.scrollTo(0, coordY);
+          clearInterval(scroller);
+        } else if (scrollBy < window.pageYOffset - coordY) {
+          window.scrollBy(0, -scrollBy);
+        } else if (scrollBy > window.pageYOffset - coordY) {
+          window.scrollBy(0, scrollBy);
+        }
+
+      }, animationTime / framesCount);
+    });
+  });
+
+
+
 
 });
